d36fb60942e38e70ecb7bb0bb4fc18636430bc52
Marcin Orzechowski <marcinorz@o2.pl>
2021-03-09 10:18:51 +0100

    Add project files.

---
+++ Ogłoszenia Drobne Web App/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ogłoszenia_Drobne_Web_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Ogłoszenia Drobne Web App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ogłoszenia_Drobne_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
