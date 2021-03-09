62692c23d490fb405aa9292cef6d91089bdc9d15
Marcin Orzechowski <marcinorz@o2.pl>
2021-03-09 11:04:53 +0100

    duzo rzeczy

---
+++ Ogłoszenia Drobne Web App/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ogłoszenia_Drobne_Web_App.Data;
+
+[assembly: HostingStartup(typeof(Ogłoszenia_Drobne_Web_App.Areas.Identity.IdentityHostingStartup))]
+namespace Ogłoszenia_Drobne_Web_App.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
